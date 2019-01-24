23dc7a186185033f301e3ad1189a81da020d9df6
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-01-24 07:39:29 -0500

    Removed IgnoredResources from FillItUp/PluginData/FillItUp.cfg  Added new file: FillItUp/IgnoredResources.cfg which now contains the ignored resources, one per line.  This change was done so that the resources can be changed at runtime with a ModuleManager script  Added ability to lock resources in the Editor for this mod.  This works on single stages as well as the entire vessel  Added saving of locked settings for all stages only.  Added stock settings page to control if locked settings should be saved or not, also whether tooltips should be shown  Fixed issue where expanded stages were immediately closing if a change was made  Added tooltips

--- FillItUp/Settings/FillItUpConfigNode.cs
+++ FillItUp/Settings/FillItUpConfigNode.cs
@@ -10,47 +10,175 @@ using UnityEngine;
 
 namespace FillItUp
 {
-    public class FillItUpConfigNode
+    public class StageRes
     {
-        const string NODE = "FILLITUP";
-        const string IGNORED = "IgnoredResources";
-        private ConfigNode _node;
-        public FillItUpConfigNode(ConfigNode node)
+        public const int ALLSTAGES = -99;
+
+        public int stage;
+        public string resource;
+        public bool locked = false;
+        public bool ignored = false;
+
+
+        public StageRes(int s, string res, bool l = false, bool i = false)
         {
-            _node = node;
+            stage = s;
+            resource = res;
+            locked = l;
+            ignored = i;
         }
-
-        public void Save()
-        {
-            string fileFullPath = GetEnsuredConfigPath();
-            if (!_node.HasValue(IGNORED))
-                IgnoredResources = new string[]{ ""};
-            ConfigNode file = new ConfigNode();
-            file.AddNode(NODE, _node);
-            file.Save(fileFullPath);
-        }
-
-        #region Ignored Resource Types
-
-        public string[] IgnoredResources
+        public string Key
         {
             get
             {
-                string stored = _node.GetValue(IGNORED);
+                return stage.ToString() + ":" + resource;
+            }
+        }
+        public static string Key2(int s, string res)
+        {
+            return s.ToString() + ":" + res;
+        }
+    }
 
-                if (String.IsNullOrEmpty(stored)) return new string[0];
+    public class FillItUpConfigNode
+    {
+        const string NODE = "FILLITUP";
 
-                return stored.Split(';');
+        const string RUNTIMELOCKED = "RuntimeLockedResources";
+
+        StageRes sr;
+
+
+        private static Dictionary<string, StageRes> runtimeLockedResources;
+
+        private ConfigNode _node;
+        public FillItUpConfigNode(ConfigNode node)
+        {
+            Debug.Log("FillItUpConfigNode Instantiator");
+            _node = node;
+        }
+
+        public void FIU_Save()
+        {
+            string fileFullPath = GetEnsuredConfigPath();
+
+            Debug.Log("FillItUpConfigNode.Save");
+            if (!_node.HasValue(RUNTIMELOCKED))
+            {
+                Debug.Log("FillItUpConfigNode.Save, initting RuntimeIgnoredResources");
+                RuntimeLockedResources = new Dictionary<string, StageRes>();
+            }
+            if (!HighLogic.CurrentGame.Parameters.CustomParams<FIU>().saveGlobalLocked)
+            {
+                _node.RemoveValue(RUNTIMELOCKED);
+            }
+            ConfigNode file = new ConfigNode();
+            file.AddNode(NODE, _node);
+            file.Save(fileFullPath);
+            LockedToNode(runtimeLockedResources);
+        }
+
+#if true
+#region Ignored Resource Types
+
+        void LoadRuntimeLockedResources(bool init)
+        {
+            Debug.Log("FillItUpConfigNode.LoadRuntimeLockedResources");
+            string stored = null;
+            if (_node.HasValue(RUNTIMELOCKED))
+                stored = _node.GetValue(RUNTIMELOCKED);
+
+            if (runtimeLockedResources == null || init)
+                runtimeLockedResources =  new Dictionary<string, StageRes>();
+            if (stored != null && stored != "")
+            {
+                Debug.Log("LoadRuntimeLockedResources, stored: " + stored);
+                var r = stored.Split(';').ToList();
+                foreach (var r1 in r)
+                {
+                    sr = new StageRes(StageRes.ALLSTAGES, r1);
+                    runtimeLockedResources.Add(sr.Key, sr);
+                }
+            }
+        }
+        
+        void LockedToNode(Dictionary<string, StageRes> value)
+        {
+            string s = "";
+            foreach (var s1 in value)
+            {
+                Debug.Log("stage: " + s1.Value.stage + ", s1.Value.resource: " + s1.Value.resource);
+                if (s1.Value.stage == StageRes.ALLSTAGES)
+                {
+                    if (s != "")
+                        s += ";" + s1.Value.resource;
+                    else
+                        s = s1.Value.resource;
+                }
+            }
+            Debug.Log("RUNTIMELOCKED: " + s);
+            _node.SetValue(RUNTIMELOCKED, s, true);
+            LoadRuntimeLockedResources(true);
+        }
+
+        public Dictionary<string, StageRes> RuntimeLockedResources
+        {
+            get
+            {
+                if (runtimeLockedResources == null)
+                    LoadRuntimeLockedResources(true);
+                return runtimeLockedResources;
             }
             set
             {
-                _node.SetValue(IGNORED, String.Join(";", value), true);
+                LockedToNode(value);
+#if false
+                string s = "";
+                foreach (var s1 in value)
+                {
+                    Debug.Log("stage: " + s1.Value.stage + ", s1.Value.resource: " + s1.Value.resource);
+                    if (s1.Value.stage == StageRes.ALLSTAGES && HighLogic.CurrentGame.Parameters.CustomParams<FIU>().saveGlobalLocked)
+                    {
+                        if (s != "")
+                            s += ";" + s1.Value.resource;
+                        else
+                            s = s1.Value.resource;
+                    }
+                }
+
+                _node.SetValue(IGNORED, s, true);
+                LoadRuntimeIgnoredResources();
+#endif
+            }
+        }
+       
+        public void AddRuntimeLockedResource(int stage, string s)
+        {
+            Debug.Log("AddRuntimeLockedResource, stage: " + stage + ", res: " + s);
+            if (!runtimeLockedResources.TryGetValue(StageRes.Key2(stage, s), out sr))
+            {
+                sr = new StageRes(stage, s);
+                runtimeLockedResources.Add(sr.Key, sr);
+                LockedToNode(runtimeLockedResources);
+            }
+        }
+        public void RemoveRuntimeLockedResource(int stage, string s)
+        {
+            Debug.Log("RemoveRuntimeLockedResource, stage: " + stage + ", res: " + s);
+
+            if (runtimeLockedResources.TryGetValue(StageRes.Key2(stage, s), out sr))
+            {
+                sr = new StageRes(stage, s);
+                runtimeLockedResources.Remove(sr.Key);
+                LoadRuntimeLockedResources(false);
+                LockedToNode(runtimeLockedResources);
             }
         }
 
-        #endregion
 
-        #region Window Position
+#endregion
+#endif
+                #region Window Position
 
         public float WindowX
         {
@@ -84,12 +212,13 @@ namespace FillItUp
             }
         }
 
-        #endregion
+#endregion
 
-        #region Static
+#region Static
 
         public static FillItUpConfigNode LoadOrCreate()
         {
+            Debug.Log("FillItUpConfigNode");
             string fileFullPath = GetEnsuredConfigPath();
 
             ConfigNode file = ConfigNode.Load(fileFullPath);
@@ -123,6 +252,6 @@ namespace FillItUp
             return path;
         }
 
-        #endregion
+#endregion
     }
 }
